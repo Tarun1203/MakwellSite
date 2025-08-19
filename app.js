640bcb2eef301a2ec0805c307d5252582c376216
Tarun1203 <63863164+Tarun1203@users.noreply.github.com>
2025-08-19 15:10:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+(function(){
+  const here = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('.nav a').forEach(a=>{
+    if(a.getAttribute('href')===here) a.classList.add('active');
+  });
+})();
